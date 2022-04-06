856d6e13129424de2366f72450c8a41744f5c8a5
munnep <patrick.munne@hashicorp.com>
2022-04-06 09:46:38 +0200

    network and settings

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "tag_prefix" {
+  default = "patrick"
+}
+
+variable "region" {
+  default = "eu-west-3"
+}
+
+variable "vpc_cidr" {
+  default = "10.233.0.0/16"  
+  description = "which private subnet do you want to use for the VPC. Subnet mask of /16"
+}
+
+# variable "ami" {
+#   default     = "ami-0a49b025fffbbdac6"
+#   description = "Must be an Ubuntu image that is available in the region you choose"
+# }
